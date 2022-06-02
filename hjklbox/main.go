1309a4fb417de639f882dbe194d919de514911bc
Octopus Octopus <oct2pus@jade.moe>
2022-06-02 08:58:30 -0500

    simplify main.go

--- hjklbox/main.go
+++ hjklbox/main.go
@@ -6,12 +6,8 @@ import (
 )
 
 func main() {
-	plate := plate()
-	top := top()
-	bottom := bottom()
-	walls := walls()
-	render.ToSTL(walls, 400, "walls.stl", dc.NewDualContouringDefault())
-	render.ToSTL(bottom, 400, "bottom.stl", dc.NewDualContouringDefault())
-	render.ToSTL(top, 400, "top.stl", dc.NewDualContouringDefault())
-	render.ToSTL(plate, 400, "plate.stl", dc.NewDualContouringDefault())
+	render.ToSTL(walls(), 400, "walls.stl", dc.NewDualContouringDefault())
+	render.ToSTL(bottom(), 400, "bottom.stl", dc.NewDualContouringDefault())
+	render.ToSTL(top(), 400, "top.stl", dc.NewDualContouringDefault())
+	render.ToSTL(plate(), 400, "plate.stl", dc.NewDualContouringDefault())
 }
