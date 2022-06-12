3d9a8e9659bbfef3f00854bd2d2d4ff73aed7c62
Octopus Octopus <oct2pus@jade.moe>
2022-06-12 11:11:19 -0500

    use plane for render splitting

--- fightstick/main.go
+++ fightstick/main.go
@@ -8,12 +8,17 @@ import (
 
 func main() {
 	top := topPlane()
-	topPlanes := splitPlane()
+	walls := wallsPlane()
+	planes := make(planes)
+	planes.add(splitPlane("top", top))
+	planes.add(splitPlane("walls", walls))
 
-	render.RenderDXF(top, 300, "top.dxf")
-	render.RenderDXF(wallsPlane(), 300, "walls.dxf")
-	for k, v := range topPlanes {
-		render.RenderDXF(v, 300, k+".dxf")
+	render.RenderDXF(top, 400, "top.dxf")
+	render.RenderDXF(wallsPlane(), 400, "walls.dxf")
+	//	render.ToSTL(sdf.Extrude3D(walls, 2), 400, "walls.stl", dc.NewDualContouringDefault())
+
+	for k, v := range planes {
+		render.RenderDXF(v, 400, k+".dxf")
 		render.ToSTL(sdf.Extrude3D(v, 2), 400, k+".stl", dc.NewDualContouringDefault())
 	}
 
