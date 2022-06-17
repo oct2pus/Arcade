2811267c86bb86a26cdebaabe4de480c1775085b
Octopus Octopus <oct2pus@jade.moe>
2022-06-17 00:39:26 -0500

    planes -> parts, expand parts

--- fightstick/main.go
+++ fightstick/main.go
@@ -3,23 +3,22 @@ package main
 import (
 	"github.com/deadsy/sdfx/render"
 	"github.com/deadsy/sdfx/render/dc"
-	"github.com/deadsy/sdfx/sdf"
 )
 
 func main() {
 	top := topPlane()
 	walls := wallsPlane()
-	planes := make(planes)
-	planes.add(splitPlane("top", top))
-	planes.add(splitPlane("walls", walls))
+	parts := make(Parts)
+	parts.add(split2DPlane("top", top, 2))
+	parts.add(split2DPlane("walls", walls, 45))
 
 	render.RenderDXF(top, 400, "top.dxf")
 	render.RenderDXF(wallsPlane(), 400, "walls.dxf")
 	//	render.ToSTL(sdf.Extrude3D(walls, 2), 400, "walls.stl", dc.NewDualContouringDefault())
 
-	for k, v := range planes {
-		render.RenderDXF(v, 400, k+".dxf")
-		render.ToSTL(sdf.Extrude3D(v, 2), 400, k+".stl", dc.NewDualContouringDefault())
+	for k, v := range parts {
+		render.RenderDXF(v.SDF2, 400, k+".dxf")
+		render.ToSTL(v.SDF3, 400, k+".stl", dc.NewDualContouringDefault())
 	}
 
 }
