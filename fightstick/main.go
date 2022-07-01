b77c162ed46c3f87b45f7f4504edfd57159868fc
Octopus Octopus <oct2pus@jade.moe>
2022-06-30 23:02:07 -0500

    render new models

--- fightstick/main.go
+++ fightstick/main.go
@@ -10,16 +10,21 @@ import (
 
 func main() {
 	tops := split2DPlane(topPlane())
-	walls := split2DPlane(wallsPlane())
+	//	walls := split2DPlane(wallsPlane())
 	bottoms := split2DPlane(bottomPlane())
 	render.RenderDXF(topPlane(), 600, "top.dxf")
 	render.RenderDXF(wallsPlane(), 600, "walls.dxf")
+	render.RenderDXF(wallCorner(), 600, "wallcorner.dxf")
+	render.ToSTL(wallFrontLeft(), 400, "wallfrontleft.stl", dc.NewDualContouringDefault())
+	render.ToSTL(wallFrontRight(), 400, "wallfrontright.stl", dc.NewDualContouringDefault())
+	render.ToSTL(wallBackLeft(), 400, "wallbackleft.stl", dc.NewDualContouringDefault())
+	render.ToSTL(wallBackRight(), 400, "wallbackright.stl", dc.NewDualContouringDefault())
 	for i, ele := range tops {
 		render.ToSTL(sdf.Extrude3D(ele, 3), 400, "top-"+strconv.Itoa(i)+".stl", dc.NewDualContouringDefault())
 	}
-	for i, ele := range walls {
+	/*for i, ele := range walls {
 		render.ToSTL(sdf.Extrude3D(ele, 45), 400, "wall-"+strconv.Itoa(i)+".stl", dc.NewDualContouringDefault())
-	}
+	}*/
 	for i, ele := range bottoms {
 		render.ToSTL(sdf.Extrude3D(ele, 3), 400, "bottom-"+strconv.Itoa(i)+".stl", dc.NewDualContouringDefault())
 	}
